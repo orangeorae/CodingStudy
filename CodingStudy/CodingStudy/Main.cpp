07acbc110d4799e0df02dadef76585e14cb61777
orangeorae <orangeorae@naver.com>
2026-03-26 20:37:41 +0900

    [2026.03.25]Coding과제 - 보물찾기 게임 만들기2
    - 다차원 배열을 사용할 수 있는가
    - 배열 안에 값을 랜덤으로 설정했을 때 코드

--- CodingStudy/CodingStudy/Main.cpp
+++ CodingStudy/CodingStudy/Main.cpp
@@ -3,49 +3,61 @@ using namespace std;
 
 int main()
 {
-    int array[7][7] = { { 0,1,0,2,0,0,0 },
-                        { 0,0,0,0,0,0,0 },
-                        { 0,2,0,1,0,0,0 },
-                        { 0,0,0,0,0,2,0 },
-                        { 0,2,0,0,0,0,0 },
-                        { 0,0,0,1,0,2,0 },
-                        { 0,0,0,0,0,0,0 } };
+    int array[7][7] = { 0 };
+    int itemCount = 0;
+    int monsterCount = 0;
+    srand(time(0));
 
+    for (int i = 0; i < 7; i++) {
+
+        for (int j = 0; j < 7; j++) {
+
+            int item = rand() % 3; //0~2
+
+            if (item == 1) {
+
+                if (itemCount < 3) { // 아이템 최대 수 조절 
+                    array[i][j] = 1;
+                    itemCount++;
+                }
+                else {
+                    array[i][j] = 0;
+                }
+            }
+            else if (item == 2) {
+                if (monsterCount < 5) { // 몬스터 최대 수 조절
+                    array[i][j] = 2;
+                    monsterCount++;
+                }
+                else {
+                    array[i][j] = 0;
+                }
+            }
+        }
+    }
     cout << "보물 찾기 게임 시작" << endl;
-
-    int itemCount = 3;
-
     while (1) {
-
-        int x = 0;  int y = 0;
-
+        int x = 0, y = 0;
         cout << "X, Y 좌표를 입력하세요: ";
         cin >> x >> y;
 
-        if (x > 6 || x < 0 || y > 6 || y < 0) {
-            cout << " 0부터 6사이에 수를 입력해주세요" << endl;
-            continue;
-        }
-        else if (array[x][y] == 0) {
-            cout << "빈칸입니다 ! 다시 입력해주세요" << endl;
+        if (x < 0 || x > 6 || y < 0 || y >6) {
+            cout << "0~ 6 까지의 숫자를 적어주세요" << endl;
             continue;
         }
 
-        else if (array[x][y] == 2) {
-            cout << "몬스터 발견! 죽었습니다." << endl;
+        if (array[x][y] == 2) {
+            cout << "몬스터 발견 게임오버" << endl;
             break;
         }
 
         if (array[x][y] == 1) {
-            itemCount--;
-            cout << "아이템  당첨! " << endl;
-
-            cout << "남은 아이템의 수는 " << itemCount << "개 입니다." << endl;;
-
-            if (itemCount == 0) {
-                cout << "아이템을 전부 찾았습니다 성공 !";
-                break;
-            }
+            cout << "아이템 발견 성공! ";
+            break;
+        }
+        else if (array[x][y] == 0) {
+            cout << "빈칸입니다. 다시 입력해주세요" << endl;
+            continue;
         }
     }
 }
