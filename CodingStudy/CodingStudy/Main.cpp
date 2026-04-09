8b0ed9f92a2248c29971675dc144cfa3e0afb237
orangeorae <orangeorae@naver.com>
2026-04-09 18:05:47 +0900

    [2026.03.26]Coding과제 - 세 용사 게임 만들기 2
    - 클래스나 구조체를 정의하고, 사용할 수 있는가
    - 동적할당으로 구현한 코드

--- CodingStudy/CodingStudy/Main.cpp
+++ CodingStudy/CodingStudy/Main.cpp
@@ -2,6 +2,9 @@
 #include<string>
 using namespace std;
 
+class JaguarHunter;
+class WildPigHunter;
+
 class DragonMaster {
 public:
     string m_dmName;
@@ -13,14 +16,15 @@ public:
         cout << m_dmName << "가 소환되었습니다." << endl;
     }
 
-    void Bress() {
-        cout << m_dmName << "의 드래곤이 브레스를 내뿜어서 데미지를 입혔다." << endl;
-    }
+
+    void Bress(JaguarHunter* targetJaguar);
+    void Bress(WildPigHunter* targetWildPig);
     void TakeDamage(int damage) {
         m_hp = m_hp - damage;
         cout << m_dmName << "가 " << damage << "의 피해를 입었다." << endl;
     }
     ~DragonMaster() {
+        cout << m_dmName << "가 죽었습니다." << endl;
     }
 
 };
@@ -36,14 +40,19 @@ public:
         cout << m_jhName << "가 소환되었습니다." << endl;
     }
 
-    void Bite() {
-        cout << m_jhName << "의  재규어가 물어서 데미지를 입혔다." << endl;
+    void Bite(DragonMaster* targetDragon) {
+        cout << m_jhName << "의  재규어가 " << targetDragon->m_dmName << "를 물어서 데미지를 입혔다." << endl;
+        targetDragon->TakeDamage(30);
     }
+
+    void Bite(WildPigHunter* targetWildPig);
+
     void TakeDamage(int damage) {
         m_hp = m_hp - damage;
         cout << m_jhName << "가 " << damage << "의 피해를 입었다." << endl;
     }
     ~JaguarHunter() {
+        cout << m_jhName << "가 죽었습니다." << endl;
     }
 
 };
@@ -58,63 +67,98 @@ public:
         cout << m_phName << "가 소환되었습니다." << endl;
     }
 
-    void HeadButt() {
-        cout << m_phName << "의  멧돼지가  박치기를 해서 데미지를 입혔다." << endl;
+    void HeadButt(DragonMaster* targetDragon) {
+        cout << m_phName << "의  멧돼지가 " << targetDragon->m_dmName << "에게 박치기를 해서 데미지를 입혔다." << endl;
+        targetDragon->TakeDamage(30);
+    }
+    void HeadButt(JaguarHunter* targetJaguar) {
+        cout << m_phName << "의  멧돼지가 " << targetJaguar->m_jhName << " 박치기를 해서 데미지를 입혔다." << endl;
+        targetJaguar->TakeDamage(30);
     }
     void TakeDamage(int damage) {
         m_hp = m_hp - damage;
         cout << m_phName << "가 " << damage << "의 피해를 입었다." << endl;
     }
     ~WildPigHunter() {
+        cout << m_phName << "가 죽었습니다." << endl;
     }
 };
+
+void DragonMaster::Bress(JaguarHunter* targetJaguar) {
+    cout << m_dmName << "의  드래곤이 " << targetJaguar->m_jhName << "에게 브레스를 내뿜어 데미지를 입혔다." << endl;
+    targetJaguar->TakeDamage(30);
+}
+void DragonMaster::Bress(WildPigHunter* targetWildPig) {
+    cout << m_dmName << "의  드래곤이 " << targetWildPig->m_phName << "에게 브레스를 내뿜어 데미지를 입혔다." << endl;
+    targetWildPig->TakeDamage(30);
+}
+void JaguarHunter::Bite(WildPigHunter* targetWildPig) {
+    cout << m_jhName << "의  재규어가 " << targetWildPig->m_phName << "를 물어서 데미지를 입혔다." << endl;
+    targetWildPig->TakeDamage(30);
+}
 
 int main()
 {
     string  call;
-    DragonMaster Dragon("드래곤마스터", 100); // 소환
-    JaguarHunter Jaguar("재규어헌터", 100); // 소환 
-    WildPigHunter WildPig("멧돼지헌터", 100); // 소환 
+    DragonMaster* DragonM = new DragonMaster("드래곤마스터", 100);
+    JaguarHunter* JaguarH = new JaguarHunter("재규어헌터", 100);
+    WildPigHunter* WildPigH = new WildPigHunter("멧돼지헌터", 100);
+    cout << endl;
     cout << "***3명의 헌터가 소환 되었습니다.*** " << endl;
     while (1) {
         cout << "***누구를 선택해 공격시키겠습니까? (드래곤마스터/ 재규어헌터/ 멧돼지헌터)***" << endl;
         cout << "입력: ";
         cin >> call;
         if (call == "드래곤마스터") {
-            Dragon.Bress();
-            Jaguar.TakeDamage(30);
-            WildPig.TakeDamage(30);
+            cout << endl;
+            DragonM->Bress(JaguarH);
+            DragonM->Bress(WildPigH);
         }
         else if (call == "재규어헌터") {
-            Jaguar.Bite();
-            Dragon.TakeDamage(30);
-            WildPig.TakeDamage(30);
+            cout << endl;
+            JaguarH->Bite(DragonM);
+            JaguarH->Bite(WildPigH);
         }
         else if (call == "멧돼지헌터") {
-            WildPig.HeadButt();
-            Dragon.TakeDamage(30);
-            Jaguar.TakeDamage(30);
+            cout << endl;
+            WildPigH->HeadButt(DragonM);
+            WildPigH->HeadButt(JaguarH);
         }
-        if (Dragon.m_hp <= 0) {
-            cout << Dragon.m_dmName << "가 죽었습니다." << endl;
-        }
-        if (Jaguar.m_hp <= 0) {
-            cout << Jaguar.m_jhName << "가 죽었습니다." << endl;
-        }
-        if (WildPig.m_hp <= 0) {
-            cout << WildPig.m_phName << "가 죽었습니다." << endl;
+        else {
+            cout << "이름을 잘못 입력 하셨습니다. 다시 적어주세요" << endl;
         }
 
-        if ((Dragon.m_hp <= 0 && Jaguar.m_hp <= 0)) {
+        if ((DragonM->m_hp <= 0 && JaguarH->m_hp <= 0)) {
+            cout << endl;
+            delete DragonM;
+            delete JaguarH;
+            cout << endl;
             cout << "-----승자는 멧돼지헌터-----" << endl;
+            DragonM = nullptr;
+            JaguarH = nullptr;
+            WildPigH = nullptr;
             break;
         }
-        else if (Dragon.m_hp <= 0 && WildPig.m_hp <= 0) {
+        else if (DragonM->m_hp <= 0 && WildPigH->m_hp <= 0) {
+            cout << endl;
+            delete DragonM;
+            delete WildPigH;
+            cout << endl;
             cout << "-----승자는  재규어헌터-----" << endl;
+            DragonM = nullptr;
+            JaguarH = nullptr;
+            WildPigH = nullptr;
             break;
         }
-        else if (Jaguar.m_hp <= 0 && WildPig.m_hp <= 0) {
+        else if (JaguarH->m_hp <= 0 && WildPigH->m_hp <= 0) {
+            cout << endl;
+            delete JaguarH;
+            delete WildPigH;
+            cout << endl;
             cout << "-----승자는  드래곤헌터-----" << endl;
+            DragonM = nullptr;
+            JaguarH = nullptr;
+            WildPigH = nullptr;
             break;
         }
     }
