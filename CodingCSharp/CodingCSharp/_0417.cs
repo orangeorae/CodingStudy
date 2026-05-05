893a2d298b8f4982bb52382e98c812ec61733832
orangeorae <orangeorae@naver.com>
2026-05-05 23:47:14 +0900

    [2026.04.17]Coding과제 - 싱글턴 매니저 생성 및 Main에서 매니저의 메서드 호출하기
    - 나만의 싱글턴(Singleton) 매니저 클래스 만들기
    - 프로그램 전체에서 단 하나의 인스턴스(객체)만 생성되도록 제어하기
    - Main에서 매니저의 메서드를 호출해보기

---
+++ CodingCSharp/CodingCSharp/_0417.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingCSharp
+{
+    internal class _0417
+    {
+        public class CatManager
+        {
+            private static CatManager _instance;
+
+            public static CatManager Inst
+            {
+                get
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new CatManager();
+                    }
+                    return _instance;
+                }
+            }
+
+            public void Cat()
+            {
+                Console.WriteLine("고양이 호출");
+            }
+
+            private CatManager() { }
+        }
+        static void Main(string[] args)
+        {
+            CatManager.Inst.Cat();
+
+
+        }
+
+    }
+}
