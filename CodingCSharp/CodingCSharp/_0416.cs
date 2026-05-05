a88ca5d52d309c9eaf05020b25cc15f4ea8345de
orangeorae <orangeorae@naver.com>
2026-05-05 18:46:21 +0900

    [2026.04.16]Coding과제 - Action을 매개변수로 받아 Invoke 및 메서드 호출하기
    - Action을 파라미터(매개변수)로 전달받는 함수 만들기
    - 함수 내부에서 전달받은 Action 델리게이트 Invoke해보기
    - 다른 메서드들을 Action에 등록하고 1번 호출해보기

---
+++ CodingCSharp/CodingCSharp/_0416.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CodingCSharp
+{
+    internal class _0416
+    {
+        static int number;
+
+        public static void OrderFood(string message, Action<int> onFood)
+        {
+
+            Console.WriteLine($"[알림]{message}");
+            Console.WriteLine($"어떤 행동을 하시겠습니까?");
+            Console.WriteLine($"1. 바로 가서 음식을 가져온다 2. 10분뒤에 음식을 가져온다");
+            number = int.Parse(Console.ReadLine());
+
+            onFood?.Invoke(number);
+
+
+        }
+
+        public static void Wait(int number)
+        {
+            if (number == 1)
+            {
+                Console.WriteLine("음식이 엄청 따뜻합니다. 맛있게 먹겠습니다.");
+            }
+
+            else if (number == 2)
+            {
+                Console.WriteLine("음식이 식었습니다. 간이 배어 더 맛있어졌습니다.");
+            }
+        }
+
+        public static void AfterOrder(int number)
+        {
+            Console.WriteLine("맛있게 드셨으면 후기를 남겨주세요");
+        }
+        static void Main(string[] args)
+        {
+
+            Action<int> action = Wait;
+            action += AfterOrder;
+            OrderFood("배달이 도착했습니다.", action);
+        }
+    }
+}
