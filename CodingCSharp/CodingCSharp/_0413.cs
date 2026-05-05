08852dccff3d535ad3a2307fffebd39973e2944b
orangeorae <orangeorae@naver.com>
2026-05-05 18:30:14 +0900

    [2026.04.13]Coding과제 - 나만의 클래스를 만들고, 여러 인터페이스를 상속받아 기능을 구현하기
    - 나만의 클래스 만들기
    - 인터페이스 3개이상 만들기
    - 인터페이스를 상속 받고 사용(호출) 해보기

---
+++ CodingCSharp/CodingCSharp/_0413.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingCSharp
+{
+    internal class _0413
+    {
+        public interface IHello
+        {
+            void Hello();
+        }
+
+        public interface ICalculation
+        {
+            void Calc();
+        }
+
+        public interface ITakeCall
+        {
+            void TakeCall();
+        }
+        class Counter : IHello, ICalculation, ITakeCall
+        {
+            public void Hello()
+            {
+                Console.WriteLine("카운터는 인사를 잘 해야한다.");
+            }
+            public void Calc()
+            {
+                Console.WriteLine("카운터는 계산을 잘 해야한다.");
+            }
+            public void TakeCall()
+            {
+                Console.WriteLine("카운터는 전화도 받아야한다.");
+            }
+            public void Smile()
+            {
+                Console.WriteLine("카운터는 친절해야한다.");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            counter.Hello();
+            counter.Smile();
+            counter.Calc();
+            counter.TakeCall();
+        }
+    }
+}
